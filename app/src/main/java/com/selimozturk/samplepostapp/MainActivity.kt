21c9810db9af623375a7eb52605bb2c0de98b002
ozturkselim99 <assessment-118@external.com.tr>
2022-06-18 16:00:06 +0300

    initial commit

---
+++ app/src/main/java/com/selimozturk/samplepostapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.selimozturk.samplepostapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
